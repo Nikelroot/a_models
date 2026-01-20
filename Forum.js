c468ec6746bd347c0e6537b972e83a4aaca4cde6
Nikelroot <nikel.root@gmail.com>
2026-01-20 19:22:26 +0500

    1

--- Forum.js
+++ Forum.js
@@ -1,26 +1,28 @@
 import mongoose from './lib/mongoose.js';
 
-const {Schema} = mongoose;
+const { Schema } = mongoose;
 
 const schema = new Schema(
   {
     title: String,
     href: String,
+    magnet_link: String,
     date: String,
     cat: String,
-    hasProblem: {type: Boolean, default: false},
+    hasProblem: { type: Boolean, default: false },
+    hasLink: { type: Boolean, default: false }
   },
   {
-    toObject: {virtuals: true},
-    toJSON: {virtuals: true},
-  },
+    toObject: { virtuals: true },
+    toJSON: { virtuals: true }
+  }
 );
 
-schema.index({title: 1});
-schema.index({cat: 1});
-schema.index({date: 1});
-schema.index({href: 1});
-schema.index({hasProblem: 1});
+schema.index({ title: 1 });
+schema.index({ cat: 1 });
+schema.index({ date: 1 });
+schema.index({ href: 1 });
+schema.index({ hasProblem: 1 });
 
 const Forum = mongoose.model('Forum', schema);
 export default Forum;
