7c73905a25ae7b4c4924469abfbc9f7dbcdb71e8
Nikelroot <nikel.root@gmail.com>
2026-01-20 12:16:04 +0500

    1

---
+++ Forum.js
@@ -0,0 +1,26 @@
+import mongoose from './lib/mongoose.js';
+
+const {Schema} = mongoose;
+
+const schema = new Schema(
+  {
+    title: String,
+    href: String,
+    date: String,
+    cat: String,
+    hasProblem: {type: Boolean, default: false},
+  },
+  {
+    toObject: {virtuals: true},
+    toJSON: {virtuals: true},
+  },
+);
+
+schema.index({title: 1});
+schema.index({cat: 1});
+schema.index({date: 1});
+schema.index({href: 1});
+schema.index({hasProblem: 1});
+
+const Forum = mongoose.model('Forum', schema);
+export default Forum;
